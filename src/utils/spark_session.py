3b778c2d65fbd8f9a0c37d5333824f3f05a85599
Arjun Pillai <arjunpillai@Arjuns-MacBook-Air.local>
2026-04-05 10:16:44 -0400

    Get error in loading spark session

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -1,7 +1,7 @@
 import os
 from pyspark.sql import SparkSession
 def get_spark():
-    os.environ['JAVA_HOME'] = '/opt/homebrew/opt/openjdk@17'
+    os.environ['JAVA_HOME'] = '/opt/homebrew/Cellar/openjdk@17/17.0.18/libexec/openjdk.jdk/Contents/Home'
 
     spark = (
         SparkSession.builder
@@ -9,8 +9,8 @@ def get_spark():
         .config('spark.jars.packages', 'io.delta:delta-spark_2.12:3.1.0')
         .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
         .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
-        .config('spark.driver.extraJavaOptions', '-Dlog4j.rootCategory=WARN, console')
+        .config('spark.driver.extraJavaOptions', '-Dlog4j.rootCategory=WARN,console')
         .getOrCreate()
     )
-    spark.sparkContext.setLogLevel('WARM')
+    spark.sparkContext.setLogLevel('WARN')
     return spark
